20a8bf66ba3f95f3c0e083e6a911192fa9922803
higorhms <higorhmscebrac@hotmail.com>
2020-06-30 21:33:35 -0300

    Default Structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+};
+
+export default App;
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn = () => {
+  return <View />;
+};
+
+export default SignIn;

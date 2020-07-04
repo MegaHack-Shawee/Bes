c05348ae24131fe1d2fa39db23f5138ce6f20ebe
higorhms <higorhmscebrac@hotmail.com>
2020-07-04 17:18:15 -0300

    SignUp page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,34 +1,80 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
+import {StatusBar} from 'react-native';
 
-import Container from '../../components/Container';
+import splash from '../../assets/images/splash.png';
+import logo from '../../assets/icons/splash_icon.png';
+
 import Form from '../../components/Form';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import Text from '../../components/Text';
+import {Container, LogoView, FormView, Logo} from './styles';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const SignUp = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [name, setName] = useState('');
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
-    function handleSignUp() {
+    const handleSignUp = useCallback(() => {
         navigation.replace('TabsScreen');
-    }
+    }, [navigation]);
 
     return (
-        <Container>
-            <Form>
-                <Text>Nome:</Text>
-                <Input
-                    autoCapitalize="words"
-                    value={name}
-                    onChangeText={setName}
+        <Container source={splash}>
+            <ScrollView>
+                <StatusBar
+                    backgroundColor="#ff5300"
+                    barStyle="ligght-content"
                 />
-                <Button
-                    title="Cadastrar"
-                    disabled={isButtonDisabled}
-                    callback={handleSignUp}
-                />
-            </Form>
+                <LogoView>
+                    <Logo source={logo} />
+                </LogoView>
+                <FormView>
+                    <Form>
+                        <Input
+                            autoCapitalize="none"
+                            placeholder="Nome"
+                            placeholderTextColor="#ddd"
+                            value={name}
+                            onChangeText={name => {
+                                setName(name);
+                            }}
+                        />
+                        <Input
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            placeholder="Digite seu e-mail"
+                            placeholderTextColor="#ddd"
+                            value={email}
+                            onChangeText={email => {
+                                setEmail(email);
+                            }}
+                        />
+
+                        <Input
+                            autoCapitalize="none"
+                            placeholder="Digite sua senha"
+                            placeholderTextColor="#ddd"
+                            value={password}
+                            onChangeText={password => {
+                                setPassword(password);
+                            }}
+                        />
+
+                        <Input
+                            autoCapitalize="none"
+                            placeholder="Confirme sua senha"
+                            placeholderTextColor="#ddd"
+                            value={passwordConfirmation}
+                            onChangeText={password => {
+                                setPasswordConfirmation(password);
+                            }}
+                        />
+                        <Button title="Cadastrar" callback={handleSignUp} />
+                    </Form>
+                </FormView>
+            </ScrollView>
         </Container>
     );
 };
